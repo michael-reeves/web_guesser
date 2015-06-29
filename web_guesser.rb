723cee6c3a83c491fe17c600d1a6f1575d2da1ca
Michael Reeves <mreeves68@yahoo.com>
2015-06-29 15:18:10 -0600

    I4: adds color to the background

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,24 +6,25 @@ set :secret_number, rand(100)
 
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess)
+  message, color = check_guess(guess)
   # render the ERB template and pass in the number and message
   erb :index, :locals => { :number => settings.secret_number,
-                           :message => message
+                           :message => message,
+                           :color => color
                          }
 
 end
 
 def check_guess(guess)
   if guess > settings.secret_number + 5
-    'Way too high!'
+    ['Way too high!', 'red']
   elsif guess > settings.secret_number
-    'Too high!'
+    ['Too high!', '#FF9090']
   elsif guess < settings.secret_number - 5
-    'Way too low!'
+    ['Way too low!', 'red']
   elsif guess < settings.secret_number
-    'Too low!'
+    ['Too low!', '#FF9090']
   else
-    'You got it right!'
+    ['You got it right!', 'green']
   end
 end
