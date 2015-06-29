6e7a7309606e1e9be076df8b7375ec9b6eec2308
Michael Reeves <mreeves68@yahoo.com>
2015-06-29 16:17:52 -0600

    I4: adds guess limit

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,29 +2,50 @@ require "sinatra"
 require "sinatra/reloader"
 
 
-set :secret_number, rand(100)
+def reset
+  @@guesses = 5
+  @@secret_number = rand(100)
+end
+
+reset
 
 get '/' do
   guess = params["guess"].to_i
   message, color = check_guess(guess)
-  # render the ERB template and pass in the number and message
-  erb :index, :locals => { :number => settings.secret_number,
-                           :message => message,
-                           :color => color
-                         }
+  reset if color == 'green'
 
+  # render the ERB template and pass in the number and message
+  erb :index, :locals => { :number => @@secret_number,
+                           :message => message,
+                           :color => color,
+                           :guesses => @@guesses
+                         }
 end
 
 def check_guess(guess)
-  if guess > settings.secret_number + 5
-    ['Way too high!', 'red']
-  elsif guess > settings.secret_number
-    ['Too high!', '#FF9090']
-  elsif guess < settings.secret_number - 5
-    ['Way too low!', 'red']
-  elsif guess < settings.secret_number
-    ['Too low!', '#FF9090']
+  if guess > @@secret_number + 5
+    result = ["Way too high!", 'red']
+  elsif guess > @@secret_number
+    result = ["Too high!", '#FF9090']
+  elsif guess < @@secret_number - 5
+    result = ["Way too low!", 'red']
+  elsif guess < @@secret_number
+    result = ["Too low!", '#FF9090']
   else
-    ['You got it right!', 'green']
+    @@guesses = 5
+    result = ['You got it right! A new number has been created.', 'green']
   end
+
+  @@guesses -= 1
+
+  if @@guesses == 0
+    reset
+    unless result[1] == 'green'
+      result = ['You ran out of guesses. A new number has been created.',
+                'white']
+    end
+  end
+
+  return result
 end
+
